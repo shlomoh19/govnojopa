8b37d7d070e342373ce4206d966021b460118eaf
Aren <reactDOM.render@yandex.ru>
2021-02-07 03:03:27 -0800

    07.02.2021

--- src/components/faq/faq-page.js
+++ src/components/faq/faq-page.js
@@ -1,20 +1,23 @@
 import React, {useEffect} from 'react'
 import {smoothJumpUp} from "../../utils/scroll-utils";
 import './faq-page.scss'
+import {useTranslation} from "react-i18next";
 
 const FaqPage = () => {
+    const {t} = useTranslation()
+
     useEffect(() => {
         smoothJumpUp()
     }, [])
     return (
         <div className="faq__wrapper">
-            <h1 className="faq__title">FREQUENTLY ASKED QUESTIONS</h1>
-            <p className="faq__question">At what age can I participate in casting?</p>
-            <p className="faq__answer">- You can participate if you are 18 years old or if you will be at 18 years old at the end of the casting. We do not have requirements for the upper age limited. But all those who pass the qualifying round will definitely undergo for a medical examination.</p>
-            <p className="faq__question"> People from which countries can take part in the casting?</p>
-            <p className="faq__answer">- From any country.</p>
-            <p className="faq__question">Can I take part in the casting if I am a girl?</p>
-            <p className="faq__answer">- At this moment we have an online casting for the international world team for men's football team. We are in a development phase, we will try to provide the same opportunity for women's football in the near future.</p>
+            <h1 className="faq__title">{t("faq.title")}</h1>
+            <p className="faq__question">{t("faq.q1")}</p>
+            <p className="faq__answer">{t("faq.a1")}</p>
+            <p className="faq__question">{t("faq.q2")}</p>
+            <p className="faq__answer">{t("faq.a2")}</p>
+            <p className="faq__question">{t("faq.q3")}</p>
+            <p className="faq__answer">{t("faq.a3")}</p>
         </div>
     )
 }
